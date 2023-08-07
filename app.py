3f01bd29068c8085c4b8896a96ba998120436eb3
ms0075415 <139063733+ms0075415@users.noreply.github.com>
2023-08-07 12:00:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -33,9 +33,44 @@ def callback():
 #處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
+
+    emoji = [
+            {
+                "index":0,
+                "productId":"5ac1bfd5040ab15980c9b435",
+                "emojiID":"082"
+            },
+            {
+                "index":17,
+                "productId":"5ac1bfd5040ab15980c9b435",
+                "emojiID":"082"
+            }
+        ]
+    text_message = TextSendMessage(text='''$ Master Finance $
+Hello! 您好，歡迎您成為 Master Finance的好友!
+
+我是Master財經小幫手
+
+-這裡有股票，匯率資訊喔~
+-直接點選下方【圖中】選單功能  
+
+-期待您的光臨!''', emojis=emoji)
+
+    sticker_message = StickerSendMessage(
+        package_id='11537',
+        sticker_id='52002738'
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message, sticker_message])
+
+
+
+#     message = TextSendMessage(text=event.message.text)
+#     line_bot_api.reply_message(event.reply_token, message) #回覆你輸入的訊息(重複你說的話)
 
 if __name__ =="__main__":
     app.run()
+
+
 
