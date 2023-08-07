7d1afbfda472261e41cfbec51d1da1f75fea3870
ms0075415 <139063733+ms0075415@users.noreply.github.com>
2023-08-07 15:29:22 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -31,31 +31,7 @@ def handle_message(event):
         about_us_event(event)
         Usage(event)
 
-    if event.message.text == "@小幫手":
-        buttons_template = TemplateSendMessage(
-            alt_text='小幫手 template',
-            template=ButtonsTemplate(
-                title = '請選擇服務',
-                text = '請選擇',
-                thumbnail_image_url='https://i.imgur.com/GIVVWxG.jpg',
-                actions = [
-                    MessageTemplateAction(
-                        label = '油價查詢',
-                        text = '油價查詢'
-                    ),
-                    MessageTemplateAction(
-                        label = '匯率查詢',
-                        text = '匯率查詢'
-                    ),
-                    MessageTemplateAction(
-                        label='股價查詢',
-                        text='股價查詢'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-
+    
 #     message = TextSendMessage(text=event.message.text)
 #     line_bot_api.reply_message(event.reply_token, message) #回覆你輸入的訊息(重複你說的話)
 
--- events/basic.py
+++ events/basic.py
@@ -28,9 +28,32 @@ Hello! 您好，歡迎您成為 Master Finance的好友!
         package_id='11537',
         sticker_id='52002738'
     )
+
+    buttons_template = TemplateSendMessage(
+        alt_text='小幫手 template',
+        template=ButtonsTemplate(
+            title = '請選擇服務',
+            text = '請選擇',
+            thumbnail_image_url='https://i.imgur.com/GIVVWxG.jpg',
+            actions = [
+                MessageTemplateAction(
+                    label = '油價查詢',
+                    text = '油價查詢'
+                ),
+                MessageTemplateAction(
+                    label = '匯率查詢',
+                    text = '匯率查詢'
+                ),
+                MessageTemplateAction(
+                    label='股價查詢',
+                    text='股價查詢'
+                )
+            ]
+        )
+    )
     line_bot_api.reply_message(
         event.reply_token,
-        [text_message, sticker_message])
+        [text_message, sticker_message, buttons_template])
     
 def push_msg(event, msg):
     try:
