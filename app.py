1c492003b986cb5da5a6736b0b3c259bbe3f530e
ms0075415 <139063733+ms0075415@users.noreply.github.com>
2023-08-09 10:16:34 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -2,6 +2,10 @@
 from line_bot_api import *
 from events.basic import *
 from events.oil import *
+from events.Msg_Template import *
+import re
+import twstock
+import datetime
 
 app = Flask(__name__)
 
@@ -29,6 +33,8 @@ def handle_message(event):
     profile = line_bot_api.get_profile(event.source.user_id)
     uid = profile.user_id #使用者ID
     message_text = str(event.message.text).lower()
+    msg = str(event.message.text).upper().strip() #使用者輸入的內容
+    emsg = event.message.text
    
 ############################ 使用說明 選單 最新油價############################
     if message_text == '@使用說明':
@@ -44,7 +50,46 @@ def handle_message(event):
 ############################ 使用說明 選單 股票看板 ############################
     if event.message.text == "股價查詢":
         line_bot_api.push_message(uid, TextSendMessage("👉請輸入\
-                                                       \n #加股票代號...."))
+                                                       \n # 加股票代號...."))
+    #股價查詢
+    if re.match("想知道股價[0-9]", msg):
+        stockNumber = msg[5:9]
+        btn_msg = stock_reply_other(stockNumber)
+        line_bot_api.push_message(uid, btn_msg)
+        return 0
+    if(emsg.startswith('#')):
+            text = emsg[1:]
+            content = ''
+
+            stock_rt = twstock.realtime.get(text)
+            my_datetime = datetime.datetime.fromtimestamp(stock_rt['timestamp']+8*60*60)
+            my_time = my_datetime.strftime('%H:%M:%S')
+
+            content += '%s (%s) %s\n' %(
+                stock_rt['info']['name'],
+                stock_rt['info']['code'],
+                my_time)
+            content += '現價: %s / 開盤: %s\n'%(
+                stock_rt['realtime']['latest_trade_price'],
+                stock_rt['realtime']['open'])
+            content += '最高: %s / 最低: %s\n' %(
+                stock_rt['realtime']['high'],
+                stock_rt['realtime']['low'])
+            content += '量: %s\n' %(stock_rt['realtime']['accumlate_trade_volume'])
+
+            stock = twstock.Stock(text) #twstock.Stock('2330')
+            content += '----\n'
+            content += '最近五日價格: \n'
+            price5 = stock.price[-5:][::-1]
+            date5 = stock.date[-5:][::-1]
+            for i in range(len(price5)):
+
+                content += '[%s] %s\n' %(date5[i].strftime("%Y-%m-%d"), price5[i])
+
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=content)
+            )
 
 ############################ 粉絲/封鎖 訊息狀態 ############################
 
--- events/oil.py
+++ events/oil.py
@@ -13,3 +13,17 @@ def oil_price():
     cpc = soup.select('#cpc')[0].text.replace(' ', '')
     content = '{}\n{}{}'.format(title, gas_price, cpc)
     return content
+
+
+
+# def oil_price():
+#     target_url = 'https://gas.goodlife.tw/'
+#     rs = requests.session()
+#     res = rs.get(target_url, verify=False)
+#     res.encoding = 'utf-8'
+#     soup = BeautifulSoup(res.text, 'html.parser')
+#     title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
+#     gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
+#     cpc = soup.select('#cpc')[0].text.replace(' ', '')
+#     content = '{}\n{}{}'.format(title, gas_price, cpc)
+#     return content
