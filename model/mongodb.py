23e836c8f90b9eb145555109851e9d07f1cfe9d2
ms0075415 <139063733+ms0075415@users.noreply.github.com>
2023-08-09 14:00:00 +0800

    Add files via upload

--- model/mongodb.py
+++ model/mongodb.py
@@ -1,37 +1,39 @@
 from pymongo import MongoClient
 import datetime
+from bs4 import BeautifulSoup
+import requests
 
-#Authentication Database認證資料庫
-stockDB = 'mydb'
+# Authentication Database認證資料庫
+stockDB='mydb'
 dbname = 'howard-good31'
+
 
 def constructor_stock():
     client = MongoClient("mongodb://smile70328:Forever2031@ac-elfhgej-shard-00-00.ymufszp.mongodb.net:27017,ac-elfhgej-shard-00-01.ymufszp.mongodb.net:27017,ac-elfhgej-shard-00-02.ymufszp.mongodb.net:27017/?ssl=true&replicaSet=atlas-sbs9e1-shard-0&authSource=admin&retryWrites=true&w=majority")
     db = client[stockDB]
     return db
 
-# ---------------- 更新暫存的股票名稱 ----------------
-def update_my_stock(user_name, stockNumber, condition, target_price):
-    db = constructor_stock()
+#----------------------------更新暫存的股票名稱--------------------------
+def update_my_stock(user_name,  stockNumber, condition , target_price):
+    db=constructor_stock()
     collect = db[user_name]
-    collect.update_many({"favorite_stock": stockNumber}, {'$set': {'condition': condition, "price": target_price}})
+    collect.update_many({"favorite_stock": stockNumber }, {'$set': {'condition':condition , "price": target_price}})
     content = f"股票{stockNumber}更新成功"
     return content
-
-# ---------------- 新增使用者的股票 ----------------
-def write_my_stock(userID, user_name, stockNumber, condition, target_price):
-    db = constructor_stock()
+#   -----------    新增使用者的股票       -------------
+def write_my_stock(userID, user_name, stockNumber, condition , target_price):
+    db=constructor_stock()
     collect = db[user_name]
     is_exit = collect.find_one({"favorite_stock": stockNumber})
     if is_exit != None :
-        content = update_my_stock(user_name, stockNumber, condition, target_price)
+        content = update_my_stock(user_name, stockNumber, condition , target_price)
         return content
     else:
         collect.insert_one({
                 "userID": userID,
                 "favorite_stock": stockNumber,
-                "condition": condition,
-                "price": target_price,
+                "condition" :  condition,
+                "price" : target_price,
                 "tag": "stock",
                 "date_info": datetime.datetime.now()
             })
