91bafc1a74a9e6ea62ef815fec9cfe1e648927c5
ms0075415 <139063733+ms0075415@users.noreply.github.com>
2023-08-09 10:41:05 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ def handle_message(event):
     
     #股價查詢
     if re.match("想知道股價", msg):
-        stockNumber = msg[5:9]
+        stockNumber = msg[2:6]
         btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid, btn_msg)
         return 0
